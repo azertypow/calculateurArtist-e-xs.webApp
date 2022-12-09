ca5105ee539111e83a683b756ddfe2e8de8fd34f
azertypow <azertypow@gmail.com>
2022-12-09 09:21:54 +0100

    build: v0.1

---
+++ docs/assets/HomeView.c3126c23.js
@@ -0,0 +1,1 @@
+import{A as o}from"./AppNav.ce96de44.js";import{d as n,_ as s,r as t,o as a,c as _,a as c}from"./index.7f690221.js";const p=n({name:"HomeView",components:{AppNav:o}});const r={class:"v-home-view"};function d(m,i,v,f,l,u){const e=t("app-nav");return a(),_("div",r,[c(e)])}const V=s(p,[["render",d],["__scopeId","data-v-46114d93"]]);export{V as default};
