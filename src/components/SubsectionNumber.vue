bbce819193f57adc9846fe785f310af539ed8fe2
azertypow <azertypow@gmail.com>
2023-04-21 10:48:09 +0200

    fieat: align cherry-pick dev--before branch

--- src/components/SubsectionNumber.vue
+++ src/components/SubsectionNumber.vue
@@ -4,7 +4,7 @@
   >
     <div
         v-if="multiplier && multiplier.status === 'reg'"
-        class="app-g app-g--align-end"
+        class="app-g app-g--align-center"
     >
       <div class="app-g__coll-sm-0-12 app-g__coll-2-12 app-with-gutter"></div>
       <div class="app-g__coll-xs-12-12 app-g__coll-sm-4-12 app-g__coll-4-12 app-with-gutter" >{{ multiplier.text }}</div>
@@ -39,7 +39,7 @@
     </div>
 
     <div
-        class="app-g app-g--align-end"
+        class="app-g app-g--align-center"
     >
       <div class="app-g__coll-sm-0-12 app-g__coll-2-12 app-with-gutter"></div>
       <div class="app-g__coll-xs-12-12 app-g__coll-sm-4-12 app-g__coll-4-12 app-with-gutter" >
