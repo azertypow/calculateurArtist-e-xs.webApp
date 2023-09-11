7a99f364114faf15bf9b69c302b20b1e6e9802b8
azertypow <azertypow@gmail.com>
2023-09-11 15:55:14 +0200

    feat: option et non choix des option

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -71,7 +71,7 @@ export const useGlobalStore = defineStore('globalStore', {
                     {index : 3, title : 'Œuvres', required : true,
 help:`
 <p>Préciser la nature des œuvres présentées pour l'exposition, selon trois critères possibles. Si différentes œuvres entrent dans différentes catégories, choisir l'option la plus avantageuse pour l'artiste.
-Les choix des options sont classées de la moins avantageuse à la plus avantageuse pour l'artiste.</p>
+Les options sont classées de la moins avantageuse à la plus avantageuse pour l'artiste.</p>
 
 <p>Exemple : Si une exposition regroupe des œuvres déjà existantes et des nouvelles créations d'un artiste, alors c’est la catégorie «conception d'une ou plusieurs nouvelles œuvres» qui est appliquée.</p>
 `}
