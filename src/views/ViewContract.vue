8c3d57bde13f93668597946ceceed1a6e8ec48c1
azertypow <azertypow@gmail.com>
2023-09-11 18:29:06 +0200

    feat: contrat1 text update

--- src/views/ViewContract.vue
+++ src/views/ViewContract.vue
@@ -7,7 +7,20 @@
     </div>
     <h2 class="app-with-gutter">GÉNÉRATEUR DE CONTRAT-MODÈLE POUR UNE EXPOSITION</h2>
 
-      <p>Ce générateur de contrats-modèles est une base qui vise à clarifier et à aider à préciser les conditions de travail des artist.exs, ainsi qu’à favoriser la négociation de leurs engagements. Il cherche à prendre en compte les intérêts des deux parties au contrat. Toutefois, dans la mesure où chaque situation est particulière, vous avez la possibilité d’adapter le modèle à vos besoins. En cas de doute, n’hésitez pas à consulter une permanence ou à prendre des conseils&nbsp;juridiques.</p>
+      <p>
+          Ce générateur de contracts-modules se base sur le contrat réalisé sous mandat de la Ville et de l'État de Genève par Visarte et l'association d'avocats Lab-of-Arts.
+          Il vise à clarifier et aide à préciser les conditions de travail des artiste.x.s, ainsi qu'à favoriser la négociation de leurs engagements.
+          Il prend en compte les intérêts des deux parties au contrat.
+          Dans la mesure où chaque situation est particulière, vous avez la possibilité d'adapter le modèle à vos besoins.
+          En cas de doute, n'hésitez pas a consulter la permanence d'aide mise en place par Visarte·Genève ou à prendre des conseils juridiques.
+          Ce générateur présente 3 parties&nbsp;:
+      </p>
+      <ul>
+          <li>Le dérouleur avec espaces d'inscription</li>
+          <li>Le contrat dans son intégralité avec mise en évidence des articles relatifs à votre situation (à l'impression, seuls ceux-ci sont pris en compte.)</li>
+          <li>Les annexes</li>
+          <li></li>
+      </ul>
 
     <div class="v-view-contract__section">
       <div
