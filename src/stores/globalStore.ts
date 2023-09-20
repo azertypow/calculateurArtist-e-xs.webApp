0ea53992afee6e43c72e8508d4900fb0ee9e1059
azertypow <azertypow@gmail.com>
2023-09-20 23:10:10 +0200

    fix: le -> la oeuvre

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -184,13 +184,13 @@ help:`
     <li> Soit définir en amont un montant forfaitaire </li>
 </ul>
 
-<p>Option 2 : Si le ou les œuvres sont produites par l’artiste + une autre personne morale ou physique&nbsp;:  
+<p>Option 2 : Si la ou les œuvres sont produites par l’artiste + une autre personne morale ou physique&nbsp;:  
 <ul>
 <li> La structure rémunère elle-même l’intervenant·e·x·s externe et cela n’apparaît pas dans les calculs ci-dessous. Pour la rémunération de l’artiste, se référer à l'option&nbsp;1</li>
 <li> L’artiste rémunère les personnes morales ou physiques employées pour la réalisation de ses pièces. Envisager dans ce cas un forfait.</li>
 </ul>
 
-<p>Option 3 : Si le ou les œuvres ne sont pas produites par l’artiste, le coût de production n'entre pas dans le calcul des honoraires de l’artiste et n'apparaît donc pas dans ce calculateur.</p>
+<p>Option 3 : Si la ou les œuvres ne sont pas produites par l’artiste, le coût de production n'entre pas dans le calcul des honoraires de l’artiste et n'apparaît donc pas dans ce calculateur.</p>
 
 <p>Dans tous les cas, ces décisions doivent se prendre en accord avec la structure. </p>
 `},
