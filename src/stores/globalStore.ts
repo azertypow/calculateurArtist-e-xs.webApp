3631aec8408a1f57a5b05b1fece94d32258f9ca8
azertypow <azertypow@gmail.com>
2023-09-20 11:14:15 +0200

    feat: add content

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -315,7 +315,7 @@ help: `
                 // ----------
                 new NumberCalculatorSection(
                     {index : 9, title : "Texte", required : false, info : 'source?',
-help:`<p>Les textes commandés par la structure à l’artiste sont rémunérés pas 1000 frappes (espaces compris). La longueur du texte final est arrondi au millier de frappes le plus proche.</p>
+help:`<p>Les textes commandés par la structure à l’artiste sont rémunérés pas 1000 frappes (espaces compris). Le coût forfaitaire par 1000 frappes est calculé automatiquement selon la taille de la structure d’accueil. La longueur du texte final est arrondi au millier de frappes le plus proche.</p>
 <ul>Exemples : 
 <li>Si le texte produit contient 1312 caractères espaces compris, compter 1 occurence.</li>
 <li>Si le texte produit contient 29’979 caractères espaces compris, compter 30 occurences.</li>
