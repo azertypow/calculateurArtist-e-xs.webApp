4d02a9da2c2efc5090f79f9382fbe5fbdc116226
azertypow <azertypow@gmail.com>
2024-03-14 19:07:14 +0100

    feat: 7 info per diem

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -153,6 +153,8 @@ export const useGlobalStore = defineStore('globalStore', {
                 new NumberCalculatorSection(
                     {index : 7, title : 'Per diem',
 help:`
+<p>Les per diems ne sont pas considérés comme du salaire, et ne sont pas soumis aux cotisations sociales.</p>
+
 <p>Indemnité forfaitaire visant à dédommager l’artiste pour ses frais de repas et de déplacement lors du montage de l'exposition par exemple.</p>
 
 <p>Le nombre de jours se détermine en accord avec la structure.</p>
